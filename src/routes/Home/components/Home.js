55a95186f2e179b822c772b1fb927cb562edd0f6
KJR <kjrkjri@outlook.com>
2018-05-07 19:42:18 +0800

    new kit

---
+++ src/routes/Home/components/Home.js
@@ -0,0 +1,11 @@
+// @flow
+import React from 'react'
+import './Home.css'
+
+export const Home = () => (
+  <div>
+    <h4>Welcome!</h4>
+  </div>
+)
+
+export default Home
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+// @flow
+// We only need to import the modules necessary for initial render
+import Home from './Home'
+import GithubRepos from './GithubRepos'
+import Counter from './Counter'
+
+// Force import during development to enable Hot-Module Replacement
+// not need ?
+// if (__DEV__) {
+//   require('./Home/components/HomeView');
+//   require('./GithubRepos/container/GithubRepos');
+//   require('./Counter');
+// }
+
+export default {
+  home: Home,
+  githubRepos: GithubRepos(),
+  counter: Counter()
+}
