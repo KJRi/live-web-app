9610210c094de94b149b162004494efe89f5a2c0
KJR <kjrkjri@outlook.com>
2018-05-10 11:34:02 +0800

    add yellow theme

--- src/components/PersonalList/PersonalList.js
+++ src/components/PersonalList/PersonalList.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import styles from './PersonalList.css'
 import { Avatar, Icon } from 'antd'
+import { Link } from 'react-router-dom'
 
 type Props = {}
 type State = {}
@@ -10,11 +11,13 @@ class PersonalList extends React.PureComponent<Props, State> {
   render () {
     return (
       <div>
-        <div className={styles['list-item']}>
-          <Avatar src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' />
-          <h3>USERNAME</h3>
-          <p>description</p>
-        </div>
+        <Link to='/circle'>
+          <div className={styles['list-item']}>
+            <Avatar src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' />
+            <h3>USERNAME</h3>
+            <p>description</p>
+          </div>
+        </Link>
         <div className={styles['list-item']}><Icon type='solution' />修改信息<Icon type='right' /></div>
         <div className={styles['list-item']}><Icon type='smile-o' />我的关注<Icon type='right' /></div>
         <div className={styles['list-item']}><Icon type='like' />我的点赞<Icon type='right' /></div>
