78f68e0ae3c982ee7cab9f0e041e4ce5ebd356ed
KJR <kjrkjri@outlook.com>
2018-05-21 13:27:14 +0800

    点赞功能

--- src/routes/MyLike/components/MyLike.js
+++ src/routes/MyLike/components/MyLike.js
@@ -2,9 +2,11 @@
 import React from 'react'
 import './MyLike.css'
 import PostPage from 'components/PostPage'
+import { List, Avatar, Icon } from 'antd'
 
 type Props = {}
 type State = {
+  likelist: Array<Object>,
   postlist: Array<Object>
 }
 
@@ -12,6 +14,7 @@ class MyLike extends React.PureComponent<Props, State> {
   constructor (props: Props) {
     super(props)
     this.state = {
+      likelist: [],
       postlist: []
     }
   }
@@ -22,13 +25,22 @@ class MyLike extends React.PureComponent<Props, State> {
     })
     .then(res => res.json())
     .then(res => {
-      this.setState({
-        postlist: res
+      res && res.map(res => {
+        fetch(`/post/get?postId=${res.post}`, {
+          method: 'GET'
+        })
+        .then(res => res.json())
+        .then(res => {
+          let arr = [res]
+          this.setState((prevState, props) => ({
+            postlist: prevState.postlist.concat(arr)
+          }))
+        })
       })
     })
   }
   render () {
-    const postlist = this.state.postlist
+    const { postlist } = this.state
     return (
       <div>
         <PostPage {...{ postlist }} />
